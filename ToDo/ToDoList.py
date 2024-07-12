6d360c1995fc3e6710eb242a6c9a795eb0630a49
Madi Yessengaliyev <myessengaliyev@aol.com>
2024-07-12 19:47:19 +1000

    ToDo: added completed tasks archive

--- ToDo/ToDoList.py
+++ ToDo/ToDoList.py
@@ -12,7 +12,9 @@ def display_menu():
     print("1. View tasks")
     print("2. New task")
     print("3. Complete task")
-    print("4. Exit")
+    print("4. View completed tasks")
+    print("5. Delete all completed task")
+    print("6. Exit")
 
 
 # Read file and load any existing tasks
@@ -20,14 +22,8 @@ def load_tasks():
     if os.path.exists(TasksFile):
         with open(TasksFile, 'r') as file:
             return json.load(file)
-    return []
+    return {"tasks": [], "completed": []}
 
-
-def load_tasks():
-    if os.path.exists(TasksFile):
-        with open(TasksFile, 'r') as file:
-            return json.load(file)
-    return []
 
 def save_tasks(tasks):
     with open(TasksFile, 'w') as file:
@@ -40,13 +36,21 @@ def view_tasks(tasks):
         for index, task in enumerate(tasks, 1):
             print(f"{index}. {task}")
 
+def view_completed_tasks(tasks):
+    if not tasks["completed"]:
+        print("No completed tasks")
+    else:
+        print("Completed Tasks:")
+        for index, task in enumerate(tasks["completed"], 1):
+            print(f"{index}. {task}")
+
 def add_task(tasks):
     task = input("Enter task: ")
     tasks.append(task)
     save_tasks(tasks)
     print(f"Task '{task}' has been added")
 
-def delete_task(tasks):
+def complete_task(tasks):
     view_tasks(tasks)
     if tasks:
         try:
@@ -59,6 +63,13 @@ def delete_task(tasks):
                 print("No such task exists")
         except ValueError:
             print("Please enter a valid number.")
+
+
+def delete_completed_tasks(tasks):
+    tasks["completed"].clear()
+    save_tasks(tasks)
+    print("Completed tasks have been deleted")
+
 
 # Main function to run the app
 def main():
@@ -73,9 +84,13 @@ def main():
             elif choice == 2:
                 add_task(tasks)
             elif choice == 3:
-                delete_task(tasks)
+                complete_task(tasks)
             elif choice == 4:
-                print("Exiting the app. Goodbye!")
+                view_completed_tasks(tasks)
+            elif choice == 5:
+                delete_completed_tasks(tasks)
+            elif choice == 6:
+                print("Have a productive day!")
                 break
             else:
                 print("Invalid choice. Please enter a number between 1 and 4.")
