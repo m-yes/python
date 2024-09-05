4fde8cd878e1a6ff440adff4279ac68ec0a3e331
Madi Yessengaliyev <myessengaliyev@aol.com>
2024-09-05 18:27:02 +1000

    to do

--- ToDo/console_menu.py
+++ ToDo/console_menu.py
@@ -33,7 +33,7 @@ class ConsoleMenu:
         if not tasks:
             print("No tasks available. Please add tasks.")
         else:
-            print("Tasks:")
+            print("Your tasks:")
             for i, task in enumerate(tasks, 1):
                 print(f"{i}. {task}")
 
